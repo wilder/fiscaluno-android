828180eb67149ecc15fa70da5bed40fa9bc262cf
Wilder Pereira <wilder_roberto@hotmail.com>
2017-07-13 23:41:56 -0300

    Added intro Activity

--- app/src/main/java/com/fiscaluno/view/IntroActivity.kt
+++ app/src/main/java/com/fiscaluno/view/IntroActivity.kt
@@ -1,14 +1,20 @@
 package com.fiscaluno.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
 import com.fiscaluno.R
 
-class IntroActivity : AppCompatActivity() {
+import com.github.paolorotolo.appintro.AppIntro
+import com.github.paolorotolo.appintro.AppIntroFragment
+
+class IntroActivity : AppIntro() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setDepthAnimation()
+        addSlide(AppIntroFragment.newInstance("Conheça as Instituições", "Veja como é estudar em qualquer lugar pela visão sincera dos alunos.", R.mipmap.ic_launcher, resources.getColor(R.color.colorPrimary)))
+        addSlide(AppIntroFragment.newInstance("Avaliação", "Aqui os alunos podem expor os pontos positivos e negativos, além de avaliar detalhadamente de maneira simples pontos como Infraestrutura, Corpo Docente etc", R.mipmap.ic_launcher, resources.getColor(R.color.colorAccent)))
+        addSlide(AppIntroFragment.newInstance("Descubra os melhores lugare para fazer um curso", "Conseguimos saber qual os melhores lugares para se fazer um curso a partir das avaliações dos alunos", R.mipmap.ic_launcher, resources.getColor(R.color.colorPrimaryDark)))
+        addSlide(AppIntroFragment.newInstance("Encontre as melhores faculdades de uma categoria", "Avaliamos segurança, infraestrutura, corpo docente, comunicação etc", R.mipmap.ic_launcher,resources.getColor(R.color.white)))
+        addSlide(AppIntroFragment.newInstance("Contribua para uma educação de qualidade", "Avalie sua instituição e contribua para a melhoria dela e ajudando quem ainda não decidiu pra onde ir.", R.mipmap.ic_launcher, resources.getColor(R.color.switch_thumb_disabled_material_dark)))
     }
 }
