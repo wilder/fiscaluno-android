c5f05c3172cb11e26043c0aa77e496474fda9594
Wilder Pereira <wilder_roberto@hotmail.com>
2017-07-25 23:58:44 -0300

    Added InstitutionDetailActivity screen

---
+++ app/src/main/java/com/fiscaluno/view/InstitutionDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.fiscaluno.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.fiscaluno.R
+
+class InstitutionDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_institution_detail)
+    }
+}
