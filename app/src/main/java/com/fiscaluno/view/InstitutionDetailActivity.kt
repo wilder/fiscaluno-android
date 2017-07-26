26696c98248ac99ab62b18829d73c375b5febe76
Wilder Pereira <wilder_roberto@hotmail.com>
2017-07-26 00:16:27 -0300

    Fixed apostrophe

--- app/src/main/java/com/fiscaluno/view/InstitutionDetailActivity.kt
+++ app/src/main/java/com/fiscaluno/view/InstitutionDetailActivity.kt
@@ -4,11 +4,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 import com.fiscaluno.R
+import com.fiscaluno.model.Institution
+import kotlinx.android.synthetic.main.activity_institution_detail.*
 
 class InstitutionDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_institution_detail)
+        intent.getParcelableExtra<Institution>("institution")
     }
+
+
 }
