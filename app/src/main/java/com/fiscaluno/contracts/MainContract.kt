760c822ef7a55d7b107fd9abcf05ee4962849e71
Wilder Pereira <wilder_roberto@hotmail.com>
2017-08-14 00:38:18 -0300

    Add MainContract

---
+++ app/src/main/java/com/fiscaluno/contracts/MainContract.kt
@@ -0,0 +1,19 @@
+package com.fiscaluno.contracts
+
+import android.content.SharedPreferences
+import com.fiscaluno.model.Institution
+
+/**
+ * Created by Wilder on 14/08/17.
+ */
+interface MainContract {
+    interface View {
+        fun showTopInstitutions(institutions: List<Institution>)
+        fun showUserInstitutionInfo(userInstitution: Institution)
+    }
+    interface Presenter {
+        fun bindView(view: MainContract.View)
+        fun loadTopInstitutions()
+        fun loadUserInstitutionInfo(sharedPreferences: SharedPreferences)
+    }
+}
