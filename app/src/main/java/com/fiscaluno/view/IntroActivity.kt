b6b1a803371a456c8b4d2f7b5a40cef22e95ff2c
Wilder Pereira <wilder_roberto@hotmail.com>
2018-05-22 20:53:37 -0300

    Fix typo and improve last intro bg color

--- app/src/main/java/com/fiscaluno/view/IntroActivity.kt
+++ app/src/main/java/com/fiscaluno/view/IntroActivity.kt
@@ -27,9 +27,9 @@ class IntroActivity : AppIntro() {
         setDepthAnimation()
         addSlide(AppIntroFragment.newInstance("Conheça as Instituições", "Veja como é estudar em qualquer lugar pela visão sincera dos alunos.", R.mipmap.ic_launcher, resources.getColor(R.color.colorPrimary)))
         addSlide(AppIntroFragment.newInstance("Avaliação", "Aqui os alunos podem expor os pontos positivos e negativos, além de avaliar detalhadamente de maneira simples pontos como Infraestrutura, Corpo Docente etc", R.mipmap.ic_launcher, resources.getColor(R.color.colorAccent)))
-        addSlide(AppIntroFragment.newInstance("Descubra os melhores lugare para fazer um curso", "Conseguimos saber qual os melhores lugares para se fazer um curso a partir das avaliações dos alunos", R.mipmap.ic_launcher, resources.getColor(R.color.colorPrimaryDark)))
+        addSlide(AppIntroFragment.newInstance("Descubra os melhores lugares para fazer um curso", "Conseguimos saber qual os melhores lugares para se fazer um curso a partir das avaliações dos alunos", R.mipmap.ic_launcher, resources.getColor(R.color.colorPrimaryDark)))
         addSlide(AppIntroFragment.newInstance("Encontre as melhores faculdades de uma categoria", "Avaliamos segurança, infraestrutura, corpo docente, comunicação etc", R.mipmap.ic_launcher,resources.getColor(R.color.white)))
-        addSlide(AppIntroFragment.newInstance("Contribua para uma educação de qualidade", "Avalie sua instituição e contribua para a melhoria dela e ajudando quem ainda não decidiu pra onde ir.", R.mipmap.ic_launcher, resources.getColor(R.color.switch_thumb_disabled_material_dark)))
+        addSlide(AppIntroFragment.newInstance("Contribua para uma educação de qualidade", "Avalie sua instituição e contribua para a melhoria dela e ajudando quem ainda não decidiu pra onde ir.", R.mipmap.ic_launcher, resources.getColor(R.color.colorPrimaryDark)))
     }
 
     override fun onDonePressed(currentFragment: Fragment) {
