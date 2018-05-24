2d2aa224fd0384ba68d7f446decd9419ba767d09
Wilder Pereira <wilder_roberto@hotmail.com>
2018-05-23 22:57:16 -0300

    Validate selected institution

--- app/src/main/java/com/fiscaluno/rating/RatingSelectInstitutionFragment.kt
+++ app/src/main/java/com/fiscaluno/rating/RatingSelectInstitutionFragment.kt
@@ -2,6 +2,7 @@ package com.fiscaluno.rating
 
 import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -83,10 +84,14 @@ class RatingSelectInstitutionFragment : Fragment(), SelectInstitutionContract.Vi
     }
 
     override fun verifyStep(): VerificationError? {
+        if(dataManager.getInstitution() == null) {
+            return VerificationError(getString(R.string.must_select_institution))
+        }
         return null
     }
 
     override fun onError(error: VerificationError) {
+        Snackbar.make(view!!, error.errorMessage, Snackbar.LENGTH_SHORT).show()
     }
 
 }
