1341d68c5c18b8beeee28a66dfac830bc0c946d6
Wilder Pereira <wilder_roberto@hotmail.com>
2018-08-25 21:46:17 -0300

    Add click listener to institution's adapter

--- app/src/main/java/com/fiscaluno/view/adapter/InstitutionsAdapter.kt
+++ app/src/main/java/com/fiscaluno/view/adapter/InstitutionsAdapter.kt
@@ -1,18 +1,20 @@
 package com.fiscaluno.view.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import com.fiscaluno.model.Institution
+import com.fiscaluno.view.InstitutionDetailActivity
 import java.util.ArrayList
 
 class InstitutionsAdapter(override val rateableEntities: ArrayList<Institution>, context: Context) : RateableEntitiesAdapter(rateableEntities, context) {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val rateableEntity = rateableEntities[position]
+        val institution = rateableEntities[position]
 
         holder.subRareableEntityName.visibility = View.GONE
 
-        rateableEntity.let {
+        institution.let {
             holder.rateableEntityImage.setImageURI(it.imageUri)
             holder.rateableEntityName.text = it.name
             holder.averageRating.text = it.averageRating.toString()
@@ -20,10 +22,10 @@ class InstitutionsAdapter(override val rateableEntities: ArrayList<Institution>,
             holder.ratedByCount.text = it.ratedByCount.toString()
         }
 
-        //TODO: Set imagedrawable
-        //holder.rateableEntityImage.setImageDrawable()
         holder.institutionCard.setOnClickListener {
-            // TODO: Go to rateableEntities tab on rateableEntity screen
+            val intent = Intent(context, InstitutionDetailActivity::class.java)
+            intent.putExtra("institution", institution)
+            context.startActivity(intent)
         }
     }
 
