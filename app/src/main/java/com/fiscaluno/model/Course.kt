d4ab62d66a31a45e41b858e1ed1bf360649494b3
Wilder Pereira <wilder_roberto@hotmail.com>
2018-09-09 23:13:44 -0300

    Add attributes to course

--- app/src/main/java/com/fiscaluno/model/Course.kt
+++ app/src/main/java/com/fiscaluno/model/Course.kt
@@ -10,17 +10,25 @@ data class Course  (
     @SerializedName("course_type") var courseType: String = "",
     @SerializedName("course_average_rating") override var averageRating: Float = 0f,
     @SerializedName("course_rated_by_count") override var ratedByCount: Int = 0,
+    @SerializedName("course_periods") var coursePeriods: List<String> = emptyList(),
+    @SerializedName("course_monthly_value_range") var monthlyValueRange: List<Float> = emptyList(),
+    @SerializedName("course_duration") var duration: Int = 0, // duration in semester
     var id: String? = null,
     @SerializedName("institution") var institution: Institution? = null
 ) : RateableEntity() {
 
     constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readFloat(),
-            parcel.readInt(),
-            parcel.readString(),
-            parcel.readParcelable(Institution::class.java.classLoader)) {
+            name = parcel.readString(),
+            courseType = parcel.readString(),
+            averageRating = parcel.readFloat(),
+            ratedByCount = parcel.readInt(),
+            coursePeriods = parcel.createStringArrayList(),
+            duration = parcel.readInt(),
+            id = parcel.readString(),
+            institution = parcel.readParcelable(Institution::class.java.classLoader)) {
+
+        parcel.readList(monthlyValueRange, Float::class.java.classLoader)
+
     }
 
     override fun search(searchPresenter: SearchContract.Presenter, searchFilter: SearchFilter) {
@@ -33,11 +41,15 @@ data class Course  (
 
     override fun getValue() = this.name
 
+
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(name)
         parcel.writeString(courseType)
         parcel.writeFloat(averageRating)
         parcel.writeInt(ratedByCount)
+        parcel.writeStringList(coursePeriods)
+        parcel.writeList(monthlyValueRange)
+        parcel.writeInt(duration)
         parcel.writeString(id)
         parcel.writeParcelable(institution, flags)
     }
