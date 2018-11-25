4c4d6c93192785ea877bba2f98e7313f21695e15
Wilder Pereira <wilder_roberto@hotmail.com>
2018-11-25 14:20:16 -0200

    Load top institutions from firebase

--- app/src/main/java/com/fiscaluno/presenter/MainPresenter.kt
+++ app/src/main/java/com/fiscaluno/presenter/MainPresenter.kt
@@ -1,12 +1,18 @@
 package com.fiscaluno.presenter
 
+import android.support.annotation.NonNull
 import android.util.Log
 import com.fiscaluno.contracts.MainContract
+import com.fiscaluno.model.Institution
 import com.fiscaluno.network.FiscalunoApi
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.kodein.di.Kodein
 import org.kodein.di.generic.instance
+
+
 
 /**
  * Created by Wilder on 14/08/17.
@@ -15,6 +21,8 @@ class MainPresenter(val kodein: Kodein) : MainContract.Presenter {
 
     private val api: FiscalunoApi by kodein.instance()
     lateinit var view: MainContract.View
+    private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val INSTITUTIONS = "Institutions"
 
     override fun bindView(view: MainContract.View) {
         this.view = view
@@ -24,27 +32,19 @@ class MainPresenter(val kodein: Kodein) : MainContract.Presenter {
      * Loads the top rated Institutions
      */
     override fun loadTopInstitutions() {
-        //TODO: Load top Institutions
-        api.findInstitutions(sortBy = "rate", pageSize = 4)
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    when {
-                        it.code() == 401 ->
-                            //TODO: view.badRequest("Login expirado")
-                            Log.e("MainPresenter", "unable to authenticate user - 401")
-                        it.code() == 500 ->
-                            //TODO: view.badRequest("Não foi possível buscar as aulas.\nTente novamente mais tarde.")
-                            Log.e("MainPresenter", "unable to authenticate user - 500")
-                        else -> {
-                            val institutions = it.body()?.result
-                            view.showTopInstitutions(institutions)
-                        }
+
+        db.collection(INSTITUTIONS)
+                .orderBy("average_rating", Query.Direction.DESCENDING)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val institutions = task.result.toObjects(Institution::class.java)
+                        Log.d("topInstitutions", institutions.toString())
+                        view.showTopInstitutions(institutions)
+                    } else {
+                        Log.w("topInstitutions", "Error getting documents.", task.getException())
                     }
-                },{
-                    Log.e("MainPresenter", "unable to authenticate user - ${it.message}")
-                    //view.badRequest("Não foi possível buscar as aulas.\nTente novamente mais tarde.")
-                })
+                }
 
     }
 
